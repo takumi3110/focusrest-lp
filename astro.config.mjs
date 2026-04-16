9c61b1ff83587fd90c13fe227da66717f2026962
takumi3110 <takumi3110@garitto.jp>
2026-04-16 11:47:52 +0900

    remove base path and assets prefix from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,4 @@ import { defineConfig} from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://focusrest-lp.garitto.jp',
-  base: '/focusrest-lp',
-  build: {
-    assetsPrefix: '/focusrest-lp',
-  }
 });
