fa7d933b7962c20d27ba0713d0e2a387fbcec2b4
takumi3110 <takumi3110@garitto.jp>
2026-04-16 11:22:00 +0900

    enable passthrough image service in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+});
